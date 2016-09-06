47c964cc41c4b5b9b6333a7237583a0b444fc504
shogo-mitomo <shogo.mitomo@monstar-lab.com>
2016-09-06 16:57:34 +0900

    :shower: Comment out unnecessary packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,26 +28,26 @@ gem install bundler -N >/dev/null 2>&1
 
 install Git git
 install SQLite sqlite3 libsqlite3-dev
-install memcached memcached
-install Redis redis-server
-install RabbitMQ rabbitmq-server
+# install memcached memcached
+# install Redis redis-server
+# install RabbitMQ rabbitmq-server
 
-install PostgreSQL postgresql postgresql-contrib libpq-dev
-sudo -u postgres createuser --superuser vagrant
-sudo -u postgres createdb -O vagrant activerecord_unittest
-sudo -u postgres createdb -O vagrant activerecord_unittest2
+# install PostgreSQL postgresql postgresql-contrib libpq-dev
+# sudo -u postgres createuser --superuser vagrant
+# sudo -u postgres createdb -O vagrant activerecord_unittest
+# sudo -u postgres createdb -O vagrant activerecord_unittest2
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
-install MySQL mysql-server libmysqlclient-dev
-mysql -uroot -proot <<SQL
-CREATE USER 'rails'@'localhost';
-CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
-GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
-GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
-SQL
+# debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+# debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+# install MySQL mysql-server libmysqlclient-dev
+# mysql -uroot -proot <<SQL
+# CREATE USER 'rails'@'localhost';
+# CREATE DATABASE activerecord_unittest  DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+# CREATE DATABASE activerecord_unittest2 DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+# GRANT ALL PRIVILEGES ON activerecord_unittest.* to 'rails'@'localhost';
+# GRANT ALL PRIVILEGES ON activerecord_unittest2.* to 'rails'@'localhost';
+# GRANT ALL PRIVILEGES ON inexistent_activerecord_unittest.* to 'rails'@'localhost';
+# SQL
 
 install 'Nokogiri dependencies' libxml2 libxml2-dev libxslt1-dev
 install 'Blade dependencies' libncurses5-dev
